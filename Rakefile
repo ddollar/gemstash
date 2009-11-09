159e2c6149e70760c7a413a0264ef24e619b93ea
David Dollar <ddollar@gmail.com>
2009-11-08 22:57:32 -0500

    unvendor delayed::job

--- Rakefile
+++ Rakefile
@@ -3,6 +3,5 @@ require 'libdir'
 
 libdir  'common'
 libdir  'worker'
-vendor  'delayed_job-1.8.4'
 
-require 'gemcutter/worker_tasks'
+require 'gemstash_tasks'
--- worker/gemstash_tasks.rb
+++ worker/gemstash_tasks.rb
@@ -1,10 +1,8 @@
-require 'delayed_job'
 
 namespace :jobs do
 
   desc 'Run the DelayedJob worker'
   task :work do
-    Delayed::Worker.new.start
   end
   
 end
